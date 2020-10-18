9607998a7672f99713d6aaf5f28473cd8273dc28
thiago1623 <thiago.96t16@gmail.com>
2020-10-18 17:23:50 -0500

    [update]--FILE--

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - algoritm
+ * @list: ?
+ */
+void insertion_sort_list(listint_t **list)
+{
+	(void) list;
+}
---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - algoritm
+ * @list: ?
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	(void) list;
+}
