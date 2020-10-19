70716f3572c22381555b81d77925146af4b959cc
Athesto <gamez.live@gmail.com>
2020-10-19 11:43:00 -0500

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,47 +5,41 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *forward; /* runner forward */
-	listint_t *backward; /* runner backward */
-	listint_t *node; /* evaluation node */
+	listint_t *forward, *backward, *tmp1, *tmp2, *tmp3, *tmp4;
 
-	/* is a valid list or is list empty or is single item list? */
+	/* is null or is empty or is only one list */
 	if (!list || !*list || !(*list)->next)
 		return;
-	backward = *list;
-	forward = (*list)->next;
-	while (forward)
+
+
+	for (forward = *list; forward; forward = forward->next)
 	{
-		print_list(*list);
-		if (forward->n < backward->n)
+		for (backward = forward; backward->prev; backward = backward->prev)
 		{
-			node = forward;
-			forward = backward;
-			backward->next = node->next;
-			if (node->next)
-				node->next->prev = backward;
-			while (backward)
+			if (backward->n < backward->prev->n)
 			{
-				if (backward->prev == NULL)
-				{
-					node->next = backward;
-					backward->prev = node;
-					node->prev = NULL;
-					*list = node;
-					break;
-				}
-				else if (node->n > backward->prev->n)
-				{
-					node->next = backward;
-					node->prev = backward->prev;
-					backward->prev->next = node;
-					backward->prev = node;
-					break;
-				}
-				backward = backward->prev;
+				tmp1 = backward->prev->prev;
+				tmp2 = backward->prev;
+				tmp3 = backward;
+				tmp4 = backward->next;
+				if (tmp1)
+					tmp1->next = tmp3;
+				else
+					*list = tmp3;
+				tmp3->next = tmp2;
+				tmp2->next = tmp4;
+				if (tmp4)
+					tmp4->prev = tmp2;
+				tmp2->prev = tmp3;
+				tmp3->prev = tmp1;
+				backward = backward->next;
+				print_list(*list);
 			}
+			else
+				break;
 		}
-		backward = forward;
-		forward = forward->next;
 	}
 }
+
+
+
